3d433864d3e653917866319aecae9b8e8089e62d
Prince Pal <palprince3690@gmail.com>
2024-01-13 00:40:35 +0530

    initial commit

---
+++ L1.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // int d = 16;
+    // ++d;
+    
+    // cout<<d;
+
+    //precedence
+    //double->float->long long int-> long int->int->char  (high-low)
+
+    // cout<<7/2 * 5; //15
+    // cout<<5 * 7/2;  //1517  (*,/, % -> have same precedence)
+
+    //Ranges
+    // -10^9<int>10^9
+    // -10^12<long int>10^12
+    // -10^18<long long int>10^18
+   // 1e24 means 10^24  (e is a scientific way to write 10 to power something)
+
+   //ques. LIFE,Universe and Everything
+   //stop when 42 appears
+
+//    while(true){
+//     int x;
+//     cin>>x;
+//     if(x==42) break;
+//     cout<<x;
+//    }
+
+string str;
+cin>>str;
+// string str_rev;                      ///reverse string
+// for(int i=str.size()-1;i>=0;i--){
+//     str_rev.push_back(str[i]);
+    
+
+// }
+// cout<<str_rev;
+
+//check palindrome
+
+// for(int i=0;i<str.size()/2;i++){
+//     if(str[i]==str[str.size()-i-1]){
+//         cout<<"YES";
+//     }
+//     else{
+//         cout<<"NO";
+//     }
+// }
+
+}
